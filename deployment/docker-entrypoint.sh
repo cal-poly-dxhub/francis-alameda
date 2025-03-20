569422f224ca4d77e103e41a78d52c3741104bb8
spagdoon0411 <spandan.suthar@live.com>
2025-03-19 22:59:41 -0700

    fix: add sleep after docker check
    Prevents a race condition.

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -9,6 +9,7 @@ echo "Waiting for Docker daemon to start..."
 while ! docker info >/dev/null 2>&1; do
   sleep 1
 done
+sleep 2
 echo "Docker daemon started"
 
 # Set up buildx for multi-architecture builds
